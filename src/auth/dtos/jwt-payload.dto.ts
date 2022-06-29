03e0439fbdc8caa9d4b5eabcf3552aaadada5936
Shikai Wang <seuwsk@gmail.com>
2022-06-28 23:47:01 -0400

    add ApiProperty for OpenAPI enable cors changed port to 8080

--- src/auth/dtos/jwt-payload.dto.ts
+++ src/auth/dtos/jwt-payload.dto.ts
@@ -1,12 +1,16 @@
 import { IsBoolean, IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class JwtPayloadDto {
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   userId: string;
 
   @IsBoolean()
+  @ApiProperty()
   admin: boolean;
 }
--- src/auth/dtos/login-auth.dto.ts
+++ src/auth/dtos/login-auth.dto.ts
@@ -1,9 +1,12 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginAuthDto {
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   password: string;
 }
