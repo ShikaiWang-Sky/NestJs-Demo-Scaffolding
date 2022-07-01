0f34db888efdf781e49ee4aa621144dedbe34d82
Shikai Wang <seuwsk@gmail.com>
2022-06-30 22:39:25 -0400

    implement orderSuccess

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Request,
+} from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from '../decorators/public.decorator';
@@ -20,5 +28,27 @@ export class StripeController {
    */
   checkout(@Body() stripeCheckout: StripeCheckoutDto) {
     return this.stripeService.createStripeSession(stripeCheckout.priceId);
+  }
+
+  @Post('webhook')
+  @Public()
+  @ApiOperation({ summary: 'Stripe webhook' })
+  webhook(@Request() request: Request) {
+    return this.stripeService.stripeWebhook(request);
+  }
+
+  @Get('order/success')
+  @Public()
+  @ApiOperation({ summary: 'Stripe order success' })
+  orderSuccess(@Query('session_id') sessionId: string) {
+    return this.stripeService.orderSuccess(sessionId);
+  }
+
+  @Post('customer-portal')
+  @Public()
+  @ApiOperation({ summary: 'Create Stripe customer portal by customer id' })
+  createCustomerPortal(@Body() customer: any) {
+    console.log(customer);
+    return this.stripeService.createCustomerPortal(customer.id);
   }
 }
--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { StripeCheckUrlDto } from './dtos/stripe-check-url.dto';
 
@@ -22,7 +22,9 @@ export class StripeService {
       // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
       // the actual Session ID is returned in the query parameter when your customer
       // is redirected to the success page.
-      success_url: 'https://google.com',
+      // success_url:
+      //   'http://localhost:8080/order/success?session_id={CHECKOUT_SESSION_ID}',
+      success_url: 'http://localhost:3000/?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: 'https://example.com/canceled.html',
     });
     const checkout: StripeCheckUrlDto = new StripeCheckUrlDto();
@@ -31,5 +33,44 @@ export class StripeService {
     // Redirect to the URL returned on the Checkout Session.
     // With express, you can redirect with:
     //   res.redirect(303, session.url);
+  }
+
+  async stripeWebhook(request: Request) {
+    const signature = request.headers['stripe-signature'];
+    let event;
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        request.body,
+        signature,
+        this.configService.get('STRIPE_WEBHOOK_SECRET_KEY'),
+      );
+    } catch (err) {
+      throw new BadRequestException('Webhook Error: ' + err.message);
+    }
+
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        console.log(`PaymentIntent ${paymentIntent.id} succeeded.`);
+        break;
+      default:
+        console.log(`Unhandled Webhook event received: ${event.type}`);
+    }
+
+    return { status: 'success' };
+  }
+
+  async orderSuccess(sessionId: string) {
+    const session = await this.stripe.checkout.sessions.retrieve(sessionId);
+    const customer = await this.stripe.customers.retrieve(session.customer);
+    return { customer };
+  }
+
+  async createCustomerPortal(customerId: string) {
+    const portalSession = await this.stripe.billingPortal.sessions.create({
+      customer: customerId,
+      return_url: 'http://localhost:3000/',
+    });
+    return portalSession;
   }
 }
